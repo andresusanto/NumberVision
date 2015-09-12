580ac8f87b446434c8fb64d6efffa7c322c6b9f6
andresusanto <andre@andresusanto.info>
2015-09-12 20:29:37 +0700

    Project integrated with NDK

--- app/src/main/java/com/ganesus/numbervision/MainActivity.java
+++ app/src/main/java/com/ganesus/numbervision/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,27 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(test());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public native String test();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    static {
+        System.loadLibrary("numbervision");
     }
 }
---
+++ app/src/main/jni/numbervision.cpp
@@ -0,0 +1,142 @@
+// EQUALIGRAM - EQUALIZER INSTAGRAM
+// Andre Susanto, M Yafi, Ramandika P, Kevin Yudi
+// Pengcit - IF ITB
+
+#include <jni.h>
+#include <android/log.h>
+#include <stdio.h>
+#include <android/bitmap.h>
+#include <cstring>
+#include <unistd.h>
+#include <cmath>
+
+#define  LOG_TAG    "NUMBER VISION"
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define NOT_SPECIFIED_COLOR -1
+
+// supaya bisa dipanggil sama java
+extern "C"
+{
+JNIEXPORT jstring JNICALL Java_com_ganesus_numbervision_MainActivity_test (JNIEnv * env, jobject obj);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// helper class and functions
+/////////////////////////////////////////////////////////////////////////////////////
+
+class NativeBitmap{
+public:
+    uint32_t* pixels;
+    AndroidBitmapInfo bitmapInfo;
+
+    NativeBitmap(){
+        pixels = NULL;
+    }
+
+    NativeBitmap(NativeBitmap* nBitmap){
+        this->bitmapInfo = nBitmap->bitmapInfo;
+        uint32_t pixelsCount = this->bitmapInfo.height * this->bitmapInfo.width;
+
+        this->pixels = new uint32_t[pixelsCount];
+        memcpy(this->pixels, nBitmap->pixels, sizeof(uint32_t) * pixelsCount);
+    }
+};
+
+typedef struct{
+    uint8_t alpha, red, green, blue;
+} ARGB;
+
+uint32_t convertArgbToInt(ARGB argb) {
+    return (argb.alpha << 24) | (argb.red) | (argb.green << 8) | (argb.blue << 16);
+}
+
+void convertIntToArgb(uint32_t pixel, ARGB* argb){
+    argb->red = ((pixel) & 0xff);
+    argb->green = ((pixel >> 8) & 0xff);
+    argb->blue = ((pixel >> 16) & 0xff);
+    argb->alpha = ((pixel >> 24) & 0xff);
+}
+
+
+NativeBitmap* convertBitmapToNative(JNIEnv * env, jobject bitmap){
+    AndroidBitmapInfo bitmapInfo;
+    uint32_t* storedBitmapPixels = NULL;
+
+    int ret;
+    if ((ret = AndroidBitmap_getInfo(env, bitmap, &bitmapInfo)) < 0){
+        LOGE("Error eksekusi AndroidBitmap_getInfo()! error=%d", ret);
+        return NULL;
+    }
+
+    LOGD("width:%d height:%d stride:%d", bitmapInfo.width, bitmapInfo.height, bitmapInfo.stride);
+
+    if (bitmapInfo.format != ANDROID_BITMAP_FORMAT_RGBA_8888){
+        LOGE("Format bitmap bukan RGBA_8888!");
+        return NULL;
+    }
+
+    void* bitmapPixels;
+    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &bitmapPixels)) < 0){
+        LOGE("Error eksekusi AndroidBitmap_lockPixels()! error=%d", ret);
+        return NULL;
+    }
+
+    uint32_t* src = (uint32_t*) bitmapPixels;
+    storedBitmapPixels = new uint32_t[bitmapInfo.height * bitmapInfo.width];
+    uint32_t pixelsCount = bitmapInfo.height * bitmapInfo.width;
+    memcpy(storedBitmapPixels, src, sizeof(uint32_t) * pixelsCount);
+    AndroidBitmap_unlockPixels(env, bitmap);
+
+    // store ke memory sbg array int
+    NativeBitmap *nBitmap = new NativeBitmap();
+    nBitmap->bitmapInfo = bitmapInfo;
+    nBitmap->pixels = storedBitmapPixels;
+    return nBitmap;
+}
+
+jobject convertNativeToBitmap(JNIEnv * env, NativeBitmap* nBitmap){
+    if (nBitmap->pixels == NULL){
+        LOGD("Bitmap kosong / error");
+        return NULL;
+    }
+
+    // manggil fungsi bitmap java via env
+    jclass bitmapCls = env->FindClass("android/graphics/Bitmap");
+    jmethodID createBitmapFunction = env->GetStaticMethodID(bitmapCls, "createBitmap", "(IILandroid/graphics/Bitmap$Config;)Landroid/graphics/Bitmap;");
+    jstring configName = env->NewStringUTF("ARGB_8888");
+    jclass bitmapConfigClass = env->FindClass("android/graphics/Bitmap$Config");
+    jmethodID valueOfBitmapConfigFunction = env->GetStaticMethodID(bitmapConfigClass, "valueOf","(Ljava/lang/String;)Landroid/graphics/Bitmap$Config;");
+    jobject bitmapConfig = env->CallStaticObjectMethod(bitmapConfigClass, valueOfBitmapConfigFunction, configName);
+    jobject newBitmap = env->CallStaticObjectMethod(bitmapCls, createBitmapFunction, nBitmap->bitmapInfo.width, nBitmap->bitmapInfo.height, bitmapConfig);
+
+    // masukin pixel ke bitmap
+    int ret;
+    void* bitmapPixels;
+
+    if ((ret = AndroidBitmap_lockPixels(env, newBitmap, &bitmapPixels)) < 0){
+        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+        return NULL;
+    }
+
+    uint32_t* newBitmapPixels = (uint32_t*) bitmapPixels;
+    uint32_t pixelsCount = nBitmap->bitmapInfo.height * nBitmap->bitmapInfo.width;
+    memcpy(newBitmapPixels, nBitmap->pixels, sizeof(uint32_t) * pixelsCount);
+    AndroidBitmap_unlockPixels(env, newBitmap);
+
+    //LOGD("convert berhasil");
+    return newBitmap;
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+JNIEXPORT jstring JNICALL Java_com_ganesus_numbervision_MainActivity_test (JNIEnv * env, jobject obj){
+
+    return env->NewStringUTF("Coba coba gan!");
+}
